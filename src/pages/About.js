2cbaa10ba1d7865b512b5d8c7a0d8859bfdb2a24
Daniel McFadyen <shadyshadyshades@hotmail.com>
2024-02-14 16:43:11 +0800

    "buttons added"

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button, Stack } from "@mui/material";
 import Profile from "../assets/images/grad-pic-circle.png"
 
 function About() {
@@ -6,9 +6,9 @@ function About() {
     <Box>
         <img src={Profile} alt="Daniel in graduation gown" height={200}/>
         <Typography variant="h2">Hello!</Typography>
-        <Box style={{width: '70rem'}}>
+        <Box style={{width: '70rem'}} justifyContent="center">
         <Typography >
-            <br />Meet Daniel! Daniel (BA, BEd, BComp) is a computer scientist with a passion for AI, mobile application development, and the theory behind computer science.
+            Meet Daniel! Daniel (BA, BEd, BComp) is a computer scientist with a passion for AI, mobile application development, and the theory behind computer science.
             With a keen eye for detail and a knack for problem-solving, Daniel is well-equipped to tackle complex coding challenges and bring innovative ideas to life.
         </Typography>
         <Typography >
@@ -17,8 +17,15 @@ function About() {
         </Typography>
         <Typography>
             <br />As a dedicated and collaborative team player, Daniel enjoys working with others to bring projects to fruition.
-            Their strong communication skills and ability to think outside the box make them an asset to any project.
+            Their strong communication skills and ability to think outside the box make them an asset to any project.<br />
         </Typography>
+        </Box>
+        <Box paddingTop={2} display="flex" justifyContent="center" alignItems="center">
+            <Stack spacing={2} direction="row" alignItems="center">
+                <Button variant="outlined" href="https://www.linkedin.com/in/daniel-mcfadyen-curtin/">linkedin</Button>
+                <Button variant="outlined" href="https://www.instagram.com/lazyjazzfox/">instagram</Button>
+                <Button variant="outlined" href="https://github.com/shades87">github</Button>
+            </Stack>
         </Box>
     </Box>
     );
